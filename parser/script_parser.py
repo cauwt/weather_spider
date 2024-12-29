5a57b65b613d8d35cc00469f261df7c807d15d46
cauwt <cauwt@hotmail.com>
2024-12-29 14:46:14 +0800

    reorg: remove unused files

--- parser/script_parser.py
+++
@@ -1,8 +0,0 @@
-from .parser import Parser
-
-class ScriptParser(Parser):
-    
-    def __init__(self, cookie):
-        super().__init__(cookie)
-        self.url = ''
-        self.selector = None
